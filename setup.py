1dc5ea98c0991e8740712ff2be14b4adf8d79865
Bo Zhang <bozhang@nao.cas.cn>
2020-05-31 18:06:18 +0800

    version 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='songcn',
-    version='0.0.7',
+    version='0.0.8',
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description='The SONG-China data processing pipeline.', # short description
