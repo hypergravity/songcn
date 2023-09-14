97a118bcc10f40d62a5bfecbeab8a8cc764f6c92
Bo Zhang <bozhang@nao.cas.cn>
2023-09-14 17:20:47 +0800

    refactor local_max.py

--- songcn/core/trace/__init__.py
+++ songcn/core/trace/__init__.py
@@ -1,2 +1,2 @@
-from .local_max import trace_one_aperture, find_local_max
+from .local_max import trace_one_aperture, find_local_max_1d
 from .trace_for_gui import trace_local_max
--- songcn/core/trace/local_max.py
+++ songcn/core/trace/local_max.py
@@ -21,7 +21,7 @@ LocalMax = namedtuple(
 )
 
 
-def find_local_max(x, kernel_width=15) -> LocalMax:
+def find_local_max_1d(x, kernel_width=15) -> LocalMax:
     """Convolve 1d array `x` with a pulse kernel with width `kernel_width`, and find local maxima.
 
     Parameters
@@ -211,6 +211,19 @@ def trace_one_aperture(
     return ap_col, ap_mask
 
 
+# def find_local_max_2d(img, pos_init=(1, 1), extra_bin=1, kernel_width=10) -> np.ndarray:
+#     """slice by slice"""
+#     is_local_max = np.zeros_like(img, dtype=bool)
+#     for i_row in range(img.shape[0]):
+#         starting_row = i_row - extra_bin
+#         stop_row = i_row + 1 + extra_bin
+#         this_slice = img[starting_row:stop_row].sum(axis=0)
+#         localmax = find_local_max_1d(this_slice, kernel_width=kernel_width)
+#         is_local_max[i_row, localmax.max_ind] = True
+#         print(f"irow = {i_row}, {localmax.num} max found")
+#     return is_local_max
+
+
 if __name__ == "__main__":
     import os
     from astropy.io import fits
@@ -228,14 +241,14 @@ if __name__ == "__main__":
 
     print("Search for local maxima with different kernel widths")
     for kernel_width in np.arange(5, 30):
-        localmax = find_local_max(x=central_slice, kernel_width=kernel_width)
+        localmax = find_local_max_1d(x=central_slice, kernel_width=kernel_width)
         print(
             f"kernel_width: {kernel_width}, number of max: {localmax.num}, "
             f"median SNR: {np.median(localmax.max_snr):.2f}, mean SNR: {np.mean(localmax.max_snr):.2f}"
         )
 
     print("Approximately set kernel_width to 15")
-    localmax = find_local_max(x=central_slice, kernel_width=15)
+    localmax = find_local_max_1d(x=central_slice, kernel_width=15)
 
     fig, ax = plt.subplots(1, 1, figsize=(8, 8))
     ax.imshow(
